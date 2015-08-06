d6b7e5d5edf1b4fb1ddc44df8a8ad764fae8f782
lord63 <lord63.j@gmail.com>
2015-08-06 18:52:55 +0800

    Use a python module instead of json file for emoji

--- pyemojify/main.py
+++ pyemojify/main.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import
 
-from codecs import open
-import json
-from os import path
 import re
 
 import click
 
 from pyemojify import __version__
+from pyemojify.emoji import emoji_table
 
 
 @click.command(context_settings={'help_option_names': ('-h', '--help')})
 @click.version_option(__version__, '-V', '--version', message='%(version)s')
 @click.option('-t', '--text', help="The input text")
 def cli(text):
-    file_path = path.join(path.abspath(path.dirname(__file__)), 'emoji.json')
-    with open(file_path, encoding='utf-8') as f:
-        emoji_table = json.load(f)
     if not text:
         text = click.get_text_stream('stdin').read().strip()
     emoji_keys = re.findall(r':\w+:', text)
