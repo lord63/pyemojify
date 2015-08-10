bc387bd53473986c143155484a6659ea99a65983
lord63 <lord63.j@gmail.com>
2015-08-10 19:35:10 +0800

    Bug fix for :+1: and :-1:

--- pyemojify/emojify.py
+++ pyemojify/emojify.py
@@ -9,7 +9,7 @@ from pyemojify.emoji import emoji_table
 
 
 def emojify(text):
-    emoji_keys = re.findall(r':\w+:', text)
+    emoji_keys = re.findall(r':\S+:', text)
     for emoji_key in emoji_keys:
         raw_emoji = emoji_table.get(emoji_key, emoji_key)
         text = text.replace(emoji_key, raw_emoji)
